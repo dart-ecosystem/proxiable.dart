10e4513d3fa7ae348a66ad8f2d720061675c4e86
copydog <775150@qq.com>
2019-10-31 11:20:48 +0800

    package: export invocation handler

--- proxiable/lib/proxiable.dart
+++ proxiable/lib/proxiable.dart
@@ -1,6 +1,7 @@
 library proxiable;
 
 export 'package:proxiable/src/runtime/proxy.dart';
+export 'package:proxiable/src/runtime/invocation_handler.dart';
 export 'package:proxiable/src/runtime/default_invocation_handler.dart';
 
 export 'package:proxiable/src/annotation/proxiable.dart';
