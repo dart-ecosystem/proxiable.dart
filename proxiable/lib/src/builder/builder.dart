7b289f565320fb5c815f6c9bd08eb79e59d6e8d6
copydog <775150@qq.com>
2019-12-04 13:37:21 +0800

    refactor: rename writer to combiner

--- proxiable/lib/src/builder/builder.dart
+++ proxiable/lib/src/builder/builder.dart
@@ -1,6 +1,6 @@
 import 'package:build/build.dart';
 import 'package:proxiable/src/builder/collector/proxiable_collector.dart';
-import 'package:proxiable/src/builder/writer/proxiable_writer.dart';
+import 'package:proxiable/src/builder/combiner/proxiable_combiner.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder proxiableCollector(BuilderOptions options) => LibraryBuilder(
@@ -9,6 +9,6 @@ Builder proxiableCollector(BuilderOptions options) => LibraryBuilder(
     );
 
 Builder proxiableCombiner(BuilderOptions options) => LibraryBuilder(
-      ProxiableWriter(),
+      ProxiableCombiner(),
       generatedExtension: '.proxiable.dart',
     );
